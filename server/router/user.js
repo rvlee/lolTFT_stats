6f2d121d5bd4da3263cc4f1a0e5ed6c92fdefd12
Roy Lee <roylee@Roys-MacBook-Pro.local>
2019-08-24 02:57:45 -0700

    test pushing to master

--- server/router/user.js
+++ server/router/user.js
@@ -5,11 +5,6 @@ const apiKey = require('../../secret-config');
 
 const router = express.Router();
 
-// middleware that is specific to this router
-// router.use((req, res, next) => {
-//   console.log('Time: ', Date.now());
-//   next();
-// });
 
 router.get('/', (req, res) => {
   const {
