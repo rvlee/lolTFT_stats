dce8ec4949c6e116279060567da1cce5f0b23679
Andrew Lee <anxsaiyuki@gmail.com>
2019-08-27 23:39:18 -0700

    Add internal dashboard

---
+++ webpack.dashboard.config.js
@@ -0,0 +1,60 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// This file configures webpack to run on the production files
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  cache: true,
+  entry: './dashboard/src/app.jsx',
+  output: {
+    path: path.join(__dirname, '/dashboard/dist'),
+    publicPath: '/dashboard',
+    filename: 'js/app.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/dashboard/dist'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['env', 'react', 'stage-2'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[id].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  optimization: {
+    minimizer: [new TerserJSPlugin({
+
+    }), new OptimizeCSSAssetsPlugin({
+
+    })],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
